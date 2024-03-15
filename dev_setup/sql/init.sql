c9b13707dd47917d8a1ea43db5a34177d0d8127a
Abdulrahman Mosaibah <abudrm2020@gmail.com>
2024-03-15 13:31:10 +0300

    feat(db): seed the rules table

--- dev_setup/sql/init.sql
+++ dev_setup/sql/init.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE IF NOT EXISTS stock_alertify;
 
-CREATE TABLE alerts
+USE stock_alertify;
+CREATE TABLE IF NOT EXISTS alerts
 (
     id                       UUID            DEFAULT gen_random_uuid() PRIMARY KEY,
     name                     STRING NOT NULL,
@@ -9,7 +10,7 @@ CREATE TABLE alerts
     created_at               DATE   NOT NULL DEFAULT now()
 );
 
-CREATE TABLE alert_rules
+CREATE TABLE IF NOT EXISTS alert_rules
 (
     id                       UUID            DEFAULT gen_random_uuid() PRIMARY KEY,
     name                     STRING NOT NULL,
@@ -18,3 +19,8 @@ CREATE TABLE alert_rules
     is_deleted               BOOLEAN NULL,
     created_at               DATE   NOT NULL DEFAULT now()
 );
+
+
+INSERT INTO alert_rules (name, symbol, threshold_price) VALUES ('Apple Inc', 'AAPL', 173.00), ('Microsoft Corp', 'MSFT', 425.22),
+                                                               ('Alphabet Inc Class C', 'GOOG', 144.34), ('Amazon.com Inc', 'AMZN', 178.75),
+                                                               ('Meta Platforms Inc', 'META', 491.83);
