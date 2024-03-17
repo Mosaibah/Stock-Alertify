9c18de44734e977bd560f77097b9bf1b8503be23
Abdulrahman Mosaibah <abudrm2020@gmail.com>
2024-03-17 22:17:36 +0300

    docs: enhance the installation process

--- api/routes.py
+++ api/routes.py
@@ -5,7 +5,7 @@ from api.controllers.alerts_controller import  router as alerts_router
 
 def init_routes(app):
     app.include_router(market_router, prefix="/market-price", tags=["Market"])
-    app.include_router(rules_router, prefix="/alert-rules", tags=["Alert-Rules"])
+    app.include_router(rules_router, prefix="/rules", tags=["Rules"])
     app.include_router(alerts_router, prefix="/alerts", tags=["Alert"])
     return app
 
